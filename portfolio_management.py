2d6fc69c3c794689988231112a3ce000b42cd609
JohnF <johnfitz16@gmail.com>
2021-07-07 16:50:05 -0600

    fixing some syntax mistakes

--- portfolio_management.py
+++ portfolio_management.py
@@ -5,7 +5,7 @@ import pickle
 
 DEFAULT_PURCHASE_AMT = 100
 
-def trade_amount(coin1: str, coin2: str, hedge_ratio: float, long1: bool) -> Union[float, float]:
+def trade_amount(coin1: str, coin2: str, hedge_ratio: float, long1: bool, fictional: bool) -> Union[float, float]:
     """ checks positions for coin1 and coin2 to see the dollar amount of each coin that should be traded
 
     Assumes that the limiting factor is the one that we have to sell and can buy up to the remaining balance on our account.
@@ -27,7 +27,7 @@ def trade_amount(coin1: str, coin2: str, hedge_ratio: float, long1: bool) -> Uni
     float, float
         dollar values of coin1 and coin2 to purchase / sell, respectively
     """
-    portfolio = portfolio_positions()
+    portfolio = portfolio_positions(fictional=fictional)
     
     remaining_balance = portfolio['balance']
     max_budget = 0.90 * remaining_balance # trying not to exhaust all money due to slippage
@@ -51,7 +51,8 @@ def trade_amount(coin1: str, coin2: str, hedge_ratio: float, long1: bool) -> Uni
             min_amt = max_budget
         else:
             min_amt = coin1_dollar_amt
-
+    if coin1_dollar_amt ==0 or coin2_dollar_amt == 0:
+        return (0, 0)
     coin1_amt = (min_amt / coin1_dollar_amt) * coin1_coin_amt
     coin2_amt = (min_amt / coin2_dollar_amt) * coin1_coin_amt * hedge_ratio
 
@@ -80,20 +81,21 @@ def portfolio_name(fictional: bool) -> str:
     return "fictional_crypto_positions.p" if fictional else "actual_crypto_positions.p"
 
 
-def update_portfolio_positions(full_market_info: pd.DataFrame,) -> None:
+def update_portfolio_positions(market_info: pd.DataFrame,) -> None:
     """Goes through the portfolio and updates values based on coin prices"""
     actual_portfolio = portfolio_positions(fictional=False)
     fictional_portfolio = portfolio_positions(fictional=True)
 
     fictional = False
-    for portfolio in [actual_portfolio, fictional_portfolio]
+    for portfolio in [actual_portfolio, fictional_portfolio]:
         for coin in portfolio.keys():
-            try:
-                coin_price = np.exp(log[(log['coin'] == coin)]['log_close'][-1])
+            if coin != 'balance':
+                # try:
+                coin_price = market_info[market_info['coin'] == coin]['close'].iloc[-1]
                 coin_amt = portfolio[coin][0]
-                portfolio[coin] = (coin1_amt, coin1_amt*coin_price)
-            except:
-                print("can't find information for " + str(coin))
+                portfolio[coin] = (coin_amt, coin_amt*coin_price)
+                # except:
+                #     print("can't find information for " + str(coin))
         save_portfolio(fictional=fictional, portfolio=portfolio)
         fictional = True
 
@@ -117,7 +119,9 @@ def purchase_initial_position(coin_pairs: str, market_info: pd.DataFrame) -> Non
     for portfolio in [actual_portfolio, fictional_portfolio]:
         for coin in coin_list:
             if coin not in portfolio.keys():
-                coin_price = np.exp(log[(log['coin'] == coin)]['log_close'][-1])
+                print(f"adding ${DEFAULT_PURCHASE_AMT} of {coin} to {'fictional' if fictional else 'actual'} portfolio")
+                # print(len(market_info[market_info['coin'] == coin]['close']))
+                coin_price = market_info[market_info['coin'] == coin]['close'].iloc[-1]
                 portfolio[coin] = (DEFAULT_PURCHASE_AMT / coin_price, coin_price)
                 portfolio['balance'] -= DEFAULT_PURCHASE_AMT
         save_portfolio(fictional=fictional, portfolio=portfolio)
@@ -127,6 +131,7 @@ def portfolio_value(fictional: Optional[bool]=False) -> float:
     portfolio = portfolio_positions(fictional=fictional)
     portfolio_value = portfolio['balance']
     for coin in portfolio.keys():
-        portfolio_value += portfolio[coin][1]
+        if coin != 'balance':
+            portfolio_value += portfolio[coin][1]
     
     return portfolio_value
